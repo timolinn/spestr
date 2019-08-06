52f711c389b869dcef78f1734c2ca0b89cd123be
Timothy Onyiuke <timothyonyiuke@gmail.com>
2019-08-06 15:10:29 +0100

    Fetch html page and retrieve javascript filename

--- internal/fastdotcom/fastdotcom.go
+++ internal/fastdotcom/fastdotcom.go
@@ -1,4 +1,12 @@
-package fastdotcom
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 // NetworkStatus represents the summary
 // of the tested network
@@ -19,6 +27,60 @@ type FastDotCom struct {
 	Servers string
 }
 
+// RunSpeedTest interacts with fast.com to fetch
+// the Network status data and metadata
 func (fdcm FastDotCom) RunSpeedTest() (FastDotCom, error) {
+	return FastDotCom{}, nil
+}
 
+// GetHTML returns the Html representation of fast.com
+func (fdcm FastDotCom) GetHTML() (FastDotCom, error) {
+	resp, err := http.Get("https://fast.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status:", resp.Status)
+	fastCom := FastDotCom{}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	doc.Find("script").Each(func(idx int, script *goquery.Selection) {
+		println(idx)
+		node, ok := script.Attr("src")
+		if !ok {
+			log.Fatalln("Src tag missing...")
+		}
+		println(node)
+	})
+	// body, err := ioutil.ReadAll(resp.Body)
+	// stringBody := string(body)
+
+	// if num := strings.Count(stringBody, "script src"); num > 0 {
+	// 	println((num))
+	// }
+	// fmt.Println(stringBody)
+	return fastCom, nil
+}
+
+func findIpv4Addr(fqdn string) {
+
+}
+
+func findIpv6Addr(fqdn string) {
+
+}
+
+func main() {
+	fastCom := FastDotCom{}
+	res, err := fastCom.GetHTML()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(res)
 }
