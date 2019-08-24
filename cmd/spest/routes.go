9d1f58ccac679a462aa572339e28b46e36e6f664
Timothy Onyiuke <timothyonyiuke@gmail.com>
2019-08-25 00:24:33 +0100

    Refactor: separated websocket route

--- cmd/spest/routes.go
+++ cmd/spest/routes.go
@@ -2,28 +2,50 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+
 	"github.com/timolinn/spestr/internal/config"
+	"github.com/timolinn/spestr/internal/fastdotcom"
 	"github.com/timolinn/spestr/internal/home"
 	"github.com/timolinn/spestr/internal/locations"
+	"github.com/timolinn/spestr/internal/util"
 )
 
 func registerRoutes(router *gin.Engine, cfg *config.Configuration) {
 	home.HandleHome(router)
 	home.RunTest(router)
 	locations.GetLocation(router, cfg)
+
+	// Websocket route
+	router.GET("/ws", func(c *gin.Context) {
+		ws, err := util.Upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			log.Errorf("Error connecting to Websocket: %s\n", err)
+		}
+
+		fastCom := fastdotcom.FastDotCom{
+			Done: false,
+		}
+		resultChan := make(chan int)
+		go fastCom.RunSpeedTest(resultChan)
+		defer ws.Close()
+
+		for {
+			for result := range resultChan {
+				log.Printf("%d Mbps\n", result)
+				fastCom.Network.Download = result
+				// tells the browser that
+				// computation is complete
+				if result < 0 {
+					fastCom.Done = true
+				}
+				err := ws.WriteJSON(fastCom)
+				if err != nil {
+					log.Errorf("Error sending message: %v", err.Error())
+					break
+				}
+
+			}
+		}
+	})
 }
-
-// for {
-// 	msgType, msg, err := conn.ReadMessage()
-// 	if err != nil {
-// 		return
-// 	}
-
-// 	// Print the message to the console
-// 	fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-
-// 	// Write message back to browser
-// 	if err = conn.WriteMessage(msgType, msg); err != nil {
-// 		return
-// 	}
-// }
--- internal/home/homeController.go
+++ internal/home/homeController.go
@@ -5,9 +5,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/timolinn/spestr/internal/fastdotcom"
-	"github.com/timolinn/spestr/internal/util"
-
-	log "github.com/sirupsen/logrus"
 )
 
 func HandleHome(router *gin.Engine) {
@@ -15,39 +12,6 @@ func HandleHome(router *gin.Engine) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"fastCom": fastdotcom.FastDotCom{},
 		})
-	})
-
-	// Websocket route
-	router.GET("/ws", func(c *gin.Context) {
-		ws, err := util.Upgrader.Upgrade(c.Writer, c.Request, nil)
-		if err != nil {
-			log.Errorf("Error connecting to Websocket: %s\n", err)
-		}
-
-		fastCom := fastdotcom.FastDotCom{
-			Done: false,
-		}
-		resultChan := make(chan int)
-		go fastCom.RunSpeedTest(resultChan)
-		defer ws.Close()
-
-		for {
-			for result := range resultChan {
-				log.Printf("%d Mbps\n", result)
-				fastCom.Network.Download = result
-				// tells the browser that
-				// computation is complete
-				if result < 0 {
-					fastCom.Done = true
-				}
-				err := ws.WriteJSON(fastCom)
-				if err != nil {
-					log.Errorf("Error sending message: %v", err.Error())
-					break
-				}
-
-			}
-		}
 	})
 }
 
