7b9715337d9e27f3beaee5b727ff7e2dac26e3d7
Timothy Onyiuke <timothyonyiuke@gmail.com>
2019-08-27 04:53:16 +0100

    connection established

--- cmd/spest/routes.go
+++ cmd/spest/routes.go
@@ -4,11 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 
+	socketio "github.com/googollee/go-socket.io"
 	"github.com/timolinn/spestr/internal/config"
 	"github.com/timolinn/spestr/internal/fastdotcom"
 	"github.com/timolinn/spestr/internal/home"
 	"github.com/timolinn/spestr/internal/locations"
-	"github.com/timolinn/spestr/internal/util"
 )
 
 func registerRoutes(router *gin.Engine, cfg *config.Configuration) {
@@ -17,12 +17,16 @@ func registerRoutes(router *gin.Engine, cfg *config.Configuration) {
 	locations.GetLocation(router, cfg)
 
 	// Websocket route
-	router.GET("/ws", func(c *gin.Context) {
-		ws, err := util.Upgrader.Upgrade(c.Writer, c.Request, nil)
+	router.GET("/socket.io/", func(c *gin.Context) {
+		server, err := socketio.NewServer(nil)
 		if err != nil {
 			log.Errorf("Error creating websocket connection: %s\n", err)
 		}
 
+		server.OnConnect("/", func(s socketio.Conn) error {
+			log.Info("Client connected")
+			return nil
+		})
 		fastCom := fastdotcom.FastDotCom{
 			Done: false,
 		}
@@ -30,33 +34,33 @@ func registerRoutes(router *gin.Engine, cfg *config.Configuration) {
 		go func() {
 			fastCom.RunSpeedTest(resultChan)
 		}()
-		defer ws.Close()
-
-	loop:
-		for {
-			for result := range resultChan {
-				log.Printf("%d Kbps\n", result)
-				fastCom.Network.Download = result
-				// tells the browser that
-				// computation is complete
-				if result < 0 {
-					fastCom.Done = true
-					fastCom.IspInfo, _ = util.FetchISPInfo()
-					err := ws.WriteJSON(fastCom)
-					if err != nil {
-						log.Errorf("Error sending message: %v", err.Error())
-						ws.Close()
-					}
-					break loop
-				} else {
-					err := ws.WriteJSON(fastCom)
-					if err != nil {
-						log.Errorf("Error sending message: %v", err.Error())
-						ws.Close()
-						break loop
-					}
-				}
-			}
-		}
+		defer server.Close()
+		server.ServeHTTP(c.Writer, c.Request)
+		// loop:
+		// 	for {
+		// 		for result := range resultChan {
+		// 			log.Printf("%d Kbps\n", result)
+		// 			fastCom.Network.Download = result
+		// 			// tells the browser that
+		// 			// computation is complete
+		// 			if result < 0 {
+		// 				fastCom.Done = true
+		// 				fastCom.IspInfo, _ = util.FetchISPInfo()
+		// 				err := ws.WriteJSON(fastCom)
+		// 				if err != nil {
+		// 					log.Errorf("Error sending message: %v", err.Error())
+		// 					ws.Close()
+		// 				}
+		// 				break loop
+		// 			} else {
+		// 				err := ws.WriteJSON(fastCom)
+		// 				if err != nil {
+		// 					log.Errorf("Error sending message: %v", err.Error())
+		// 					ws.Close()
+		// 					break loop
+		// 				}
+		// 			}
+		// 		}
+		// 	}
 	})
 }
